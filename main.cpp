7771f732c72fe9d7c3fe54c693f9d051642a9b5f
Kindrat Beregovyi <kindratberegovyi@gmail.com>
2026-05-11 14:01:14 -0400

    Updated FEBasicApplication; The example now supports compilation to a web target using Emscripten.

--- main.cpp
+++ main.cpp
@@ -6,20 +6,21 @@ void MainWindowRender()
 	ImGui::ShowDemoWindow();
 }
 
+#ifdef _WIN32
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+#else
+int main(int argc, char** argv)
+#endif
 {
 	APPLICATION.AddWindow(1280, 720, "FEBasicApplication example");
 	APPLICATION.GetWindow(0)->SetClearColor(0.6f, 0.85f, 0.917f, 1.0f);
 	APPLICATION.GetWindow(0)->SetRenderFunction(MainWindowRender);
 
-	while (APPLICATION.IsNotTerminated())
-	{
+	APPLICATION.Run([]() {
 		APPLICATION.BeginFrame();
-
 		APPLICATION.RenderWindows();
-
 		APPLICATION.EndFrame();
-	}
+	});
 
 	return 0;
 }
